6e8e145c34925c0670c84df8595e711173a550bf
darkhuntres <52762984+darkhuntres@users.noreply.github.com>
2023-04-29 12:33:11 -0600

    conexion bd creada

---
+++ P2_2020PM608_2020DL602/Models/parcialDbContext.cs
@@ -0,0 +1,4 @@
+﻿namespace P2_2020PM608_2020DL602.Models {
+    public class parcialDbContext {
+    }
+}
--- P2_2020PM608_2020DL602/Program.cs
+++ P2_2020PM608_2020DL602/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using P2_2020PM608_2020DL602.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+//------\
+builder.Services.AddDbContext<parcialDbContext>(opt =>
+ opt.UseSqlServer(builder.Configuration.GetConnectionString("parcialDbConnection")));
 
 var app = builder.Build();
 
