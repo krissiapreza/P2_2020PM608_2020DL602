f18d6c98cf5f70429895db39435bab52e6497bf5
darkhuntres <52762984+darkhuntres@users.noreply.github.com>
2023-04-29 12:07:35 -0600

    Agregar archivos de proyecto.

---
+++ P2_2020PM608_2020DL602/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace P2_2020PM608_2020DL602.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
